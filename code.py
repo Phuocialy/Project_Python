5d2bdefb46a375b303d7738ac4c2a71142307233
Phuocialy <phuocnv2501@gmail.com>
2022-07-02 21:43:36 +0700

    code buoi 2/7

--- code.py
+++ code.py
@@ -1,6 +1,21 @@
 # Code buoi 2/7
-class Video():
- 	def __init__ (self, title, link):
- 		self.title = title
- 		self.link = link
+import module_func
 
+
+def main():
+	
+	while True:
+		module_func.show_menu()
+		choice = module_func.select_in_range("Select an option (1-7): ", 1, 7)
+		if choice == 1:
+			videos = module_func.read_videos()
+			input("\nPress Enter to continue.\n")
+		elif choice == 2:
+			module_func.print_videos(videos)
+			input("\nPress Enter to continue.\n")
+		elif choice == 3:
+			module_func.del_a_video(videos)
+		elif choice == 7:
+			break
+
+main()
